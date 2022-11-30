897e1288fd64a4018a84978f4bdc0509a2085bd0
yummyflan <phan.alanna@gmail.com>
2022-11-30 16:45:20 -0500

    changes

--- script.js
+++ script.js
@@ -1,6 +1,41 @@
 //this is our front end js// 
-function moreDetail(){
-    window.location.href="testing.html";
+function moreDetail(animal){
+    var a = parseInt(animal);
+    switch (a){
+    case (1): 
+          window.location.href="moreCapybara.html";
+          break;
+    case (2): 
+          window.location.href="moreCat.html";
+          break;
+    case (3): 
+          window.location.href="moreDog.html";
+          break;
+    case (4): 
+          window.location.href="moreGiantpanda.html";
+          break;    
+    case (5): 
+          window.location.href="moreHedgehog.html";
+          break;   
+    case (6): 
+          window.location.href="moreHorse.html";
+          break;  
+    case (7): 
+          window.location.href="morePenguin.html";
+          break;
+    case (8): 
+          window.location.href="moreRabbit.html";
+          break;
+    case (9): 
+          window.location.href="moreRedpanda.html";
+          break;
+    case (10): 
+          window.location.href="moreTiger.html";
+          break;
+    case (11): 
+          window.location.href="moreWombat.html";
+          break;
+    }
 }
 
 function home(){
